430a40c0ffebae9eea8f9f468b386c555bb814df
ProgrammerByte <tpopay@hotmail.com>
2025-01-08 11:16:49 +0000

    feat: call graph traversal

--- devaids/sqlite_test.cpp
+++ devaids/sqlite_test.cpp
@@ -26,8 +26,7 @@ void createTables() {
     CREATE TABLE nodes_table (
       funcname TEXT PRIMARY KEY,
       indegree INTEGER DEFAULT 0,
-      marked BOOLEAN DEFAULT FALSE,
-      visited BOOLEAN DEFAULT FALSE
+      marked BOOLEAN DEFAULT FALSE
     );
   )";
 
@@ -60,7 +59,7 @@ void createTables() {
 void insertExampleData() {
   const char *insertNodes = R"(
     INSERT INTO nodes_table (funcname)
-    VALUES ('t1'), ('t2'), ('t3'), ('f1'), ('f2'), ('f3'), ('f4'), ('f5'), ('f6');
+    VALUES ('t1'), ('t2'), ('t3'), ('f1'), ('f2'), ('f3'), ('f4'), ('f5'), ('f6'), ('f7');
   )";
 
   if (sqlite3_exec(db, insertNodes, nullptr, nullptr, &errMsg) != SQLITE_OK) {
@@ -73,8 +72,8 @@ void insertExampleData() {
   const char *insertAdjacency = R"(
     INSERT INTO adjacency_matrix (funcname1, funcname2)
     VALUES ('t1', 'f1'), ('t2', 'f2'), ('t3', 'f5'),
-           ('f1', 'f3'), ('f1', 'f4'), ('f2', 'f4'),
-           ('f4', 'f6'), ('f5', 'f6')
+           ('f1', 'f3'), ('f1', 'f4'), ('f2', 'f4'), ('f2', 'f6'),
+           ('f2', 'f7'), ('f4', 'f6'), ('f5', 'f6'), ('f7', 'f6')
   )";
 
   if (sqlite3_exec(db, insertAdjacency, nullptr, nullptr, &errMsg) !=
@@ -87,53 +86,157 @@ void insertExampleData() {
   }
 }
 
-void createOrdering(std::vector<std::string> &startNodes) {
-  std::string startNodesList = "('" + startNodes[0] + "'";
-  for (size_t i = 1; i < startNodes.size(); ++i) {
-    startNodesList += ", '" + startNodes[i] + "'";
+std::string createTupleList(std::vector<std::string> &nodes) {
+  std::string tupleList = "(";
+  for (size_t i = 0; i < nodes.size(); ++i) {
+    tupleList += "?";
+    if (i != nodes.size() - 1) {
+      tupleList += ", ";
+    }
   }
-  startNodesList += ")";
-  std::cout << "Start nodes list: " << startNodesList << std::endl;
+  tupleList += ")";
+  return tupleList;
+}
 
-  std::string markStartNodes = R"(
-    UPDATE nodes_table
-    SET marked = 1
-    WHERE funcname IN )" + startNodesList +
-                               ";";
-
-  if (sqlite3_exec(db, markStartNodes.c_str(), nullptr, nullptr, &errMsg) !=
-      SQLITE_OK) {
-    std::cerr << "Error marking start nodes" << errMsg << std::endl;
-    sqlite3_free(errMsg);
-  } else {
-    std::cout << "Marked start nodes successfully" << std::endl;
-  }
-
-  std::string visit = R"(
-    WITH RECURSIVE visit(start, end) AS (
-      SELECT funcname1, funcname2 FROM adjacency_matrix WHERE funcname1 IN
-        )" + startNodesList +
-                      R"(
-      UNION
-      SELECT am.funcname1, am.funcname2 FROM adjacency_matrix am
-      JOIN visit v ON am.funcname1 = v.end
-    )
-    UPDATE nodes_table
-    SET indegree = (
-      SELECT COUNT(*)
-      FROM visit
-      WHERE visit.end = nodes_table.funcname
-    ),
-    marked = 1 WHERE funcname IN (SELECT end FROM visit);
-  )";
-
-  std::cout << "Visit query: " << visit << std::endl;
-
-  if (sqlite3_exec(db, visit.c_str(), nullptr, nullptr, &errMsg) != SQLITE_OK) {
-    std::cerr << "Error preparing statement: " << errMsg << std::endl;
-    sqlite3_free(errMsg);
+void prepareStatement(sqlite3_stmt *&stmt, std::string query,
+                      std::vector<std::string> &params) {
+  if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
+    std::cerr << "Error preparing statement: " << sqlite3_errmsg(db)
+              << std::endl;
     return;
   }
+  for (int i = 0; i < params.size(); i++) {
+    if (sqlite3_bind_text(stmt, i + 1, params[i].c_str(), -1, SQLITE_STATIC) !=
+        SQLITE_OK) {
+      std::cerr << "Error binding parameter: " << sqlite3_errmsg(db) << i + 1
+                << std::endl;
+      return;
+    }
+  }
+}
+
+void prepareStatement(sqlite3_stmt *&stmt, std::string query) {
+  if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
+    std::cerr << "Error preparing statement: " << sqlite3_errmsg(db)
+              << std::endl;
+  }
+}
+
+void runStatement(sqlite3_stmt *stmt) {
+  if (sqlite3_step(stmt) != SQLITE_DONE) {
+    std::cerr << "Error running statement: " << sqlite3_errmsg(db) << std::endl;
+  }
+  sqlite3_finalize(stmt);
+}
+
+void markNodes(std::vector<std::string> &startNodes) {
+  std::vector<std::string> q = {};
+
+  for (const auto &node : startNodes) {
+    q.push_back(node);
+  }
+
+  sqlite3_stmt *stmt;
+
+  while (!q.empty()) {
+    std::string query = "UPDATE nodes_table SET marked = 1 WHERE funcname "
+                        "IN " +
+                        createTupleList(q) + ";";
+    prepareStatement(stmt, query, q);
+    runStatement(stmt);
+
+    query =
+        "WITH neighbors AS ("
+        " SELECT funcname1 AS funcname, COUNT(*) AS cnt FROM adjacency_matrix "
+        " JOIN nodes_table ON nodes_table.funcname = "
+        " adjacency_matrix.funcname1 WHERE adjacency_matrix.funcname2 IN " +
+        createTupleList(q) +
+        " GROUP BY funcname1"
+        "),"
+        "result AS ("
+        " SELECT neighbors.funcname FROM neighbors"
+        " INNER JOIN nodes_table ON nodes_table.funcname = neighbors.funcname"
+        " WHERE marked = 0"
+        ")"
+        "UPDATE nodes_table "
+        "SET indegree = indegree + COALESCE(("
+        " SELECT cnt FROM neighbors WHERE neighbors.funcname = "
+        " nodes_table.funcname"
+        "), 0);";
+
+    prepareStatement(stmt, query, q);
+    runStatement(stmt);
+
+    query =
+        "SELECT funcname FROM nodes_table WHERE marked = 0 AND indegree > 0;";
+
+    prepareStatement(stmt, query);
+
+    q.clear();
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+      std::string neighbor =
+          reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0));
+      q.push_back(neighbor);
+    }
+    sqlite3_finalize(stmt);
+  }
+}
+
+std::vector<std::string> getNextNodes(std::vector<std::string> &order) {
+  sqlite3_stmt *stmt;
+  std::string query =
+      "SELECT funcname FROM nodes_table WHERE marked = 1 AND indegree = 0;";
+  prepareStatement(stmt, query);
+  std::vector<std::string> q = {};
+  while (sqlite3_step(stmt) == SQLITE_ROW) {
+    std::string node =
+        reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0));
+    order.push_back(node);
+    q.push_back(node);
+  }
+  sqlite3_finalize(stmt);
+
+  query = "UPDATE nodes_table SET marked = 0 WHERE funcname "
+          "IN " +
+          createTupleList(q) + ";";
+  prepareStatement(stmt, query, q);
+  runStatement(stmt);
+
+  return q;
+}
+
+std::vector<std::string> traverseGraph() {
+  std::vector<std::string> order = {};
+  std::vector<std::string> q = getNextNodes(order);
+
+  while (!q.empty()) {
+    sqlite3_stmt *stmt;
+    std::string query =
+        "WITH neighbors AS ("
+        " SELECT funcname1 AS funcname, COUNT(*) AS cnt FROM adjacency_matrix "
+        " JOIN nodes_table ON nodes_table.funcname = "
+        " adjacency_matrix.funcname1 WHERE adjacency_matrix.funcname2 IN " +
+        createTupleList(q) +
+        " GROUP BY funcname1"
+        "),"
+        "result AS ("
+        " SELECT neighbors.funcname FROM neighbors"
+        " INNER JOIN nodes_table ON nodes_table.funcname = neighbors.funcname"
+        " WHERE marked = 1"
+        ")"
+        "UPDATE nodes_table "
+        "SET indegree = indegree - COALESCE(("
+        " SELECT cnt FROM neighbors WHERE neighbors.funcname = "
+        " nodes_table.funcname"
+        "), 0);";
+
+    prepareStatement(stmt, query, q);
+    runStatement(stmt);
+
+    q = getNextNodes(order);
+  }
+
+  return order;
 }
 
 // compile with the following:
@@ -146,10 +249,22 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
+  sqlite3_config(
+      SQLITE_CONFIG_LOG,
+      [](void *, int errCode, const char *msg) {
+        std::cerr << "SQLite Log [" << errCode << "]: " << msg << std::endl;
+      },
+      nullptr);
+
   createTables();
   insertExampleData();
-  std::vector<std::string> startNodes = {"t1", "t2"};
-  createOrdering(startNodes);
+  std::vector<std::string> startNodes = {"f1", "f6"};
+  markNodes(startNodes);
+  std::vector<std::string> order = traverseGraph();
+  std::cout << "Ordering:" << std::endl;
+  for (int i = 0; i < order.size(); i++) {
+    std::cout << order[i] << std::endl;
+  }
 
   sqlite3_close(db);
 }
